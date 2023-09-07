1f9a418f8544e510a6fb272cdfe37a36c325c99d
BenR <90284651+AsianUser@users.noreply.github.com>
2023-09-06 20:57:00 -0700

    added hash Method to Blob
    copy pasted Sha1 Hash code and adjusted accordingly

--- Blob.java
+++ Blob.java
@@ -27,6 +27,40 @@ public class Blob {
         File theDir = new File(objectsFolderPath, "objects");
         theDir.mkdirs();
 
+        // hash
+        String hash = hash(new File(filePath));
+
+    }
+
+    public String hash(File file) throws Exception {
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            FileInputStream fis = new FileInputStream(file);
+            byte[] dataBytes = new byte[1024];
+
+            int nread = 0;
+
+            while ((nread = fis.read(dataBytes)) != -1) {
+                md.update(dataBytes, 0, nread);
+            }
+            ;
+
+            byte[] mdbytes = md.digest();
+
+            // convert the byte to hex format
+            StringBuffer sb = new StringBuffer("");
+            for (int i = 0; i < mdbytes.length; i++) {
+                sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16)
+                        .substring(1));
+            }
+
+            fis.close();
+
+            return sb.toString();
+        } catch (NoSuchAlgorithmException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
     public static void main(String[] args) throws Exception {
