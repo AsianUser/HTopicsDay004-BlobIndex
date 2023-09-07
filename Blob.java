606561a27765a6f03a1fe65adef40cf1f827fbf3
BenR <90284651+AsianUser@users.noreply.github.com>
2023-09-07 15:26:56 -0700

    init Method and testing Index
    Added getHashString() method to Blob

--- Blob.java
+++ Blob.java
@@ -17,7 +17,7 @@ public class Blob {
         // takes path & locates parent's path
         // removes chars until locates "\\"
         int i = filePath.length() - 1;
-        while (filePath.charAt(i) != '\\') {
+        while (filePath.charAt(i) != '\\' || i >= 0) {
             // System.out.println(path.charAt(i));
             i--;
         }
@@ -26,8 +26,8 @@ public class Blob {
         String objectsFolderPath = filePath.substring(0, i);
 
         // creates folder if does not exist
-        File theDir = new File(objectsFolderPath, "objects");
-        theDir.mkdirs();
+        File objectsFolder = new File(objectsFolderPath, "objects");
+        objectsFolder.mkdirs();
 
         // hash
         hashFileString = hash(new File(filePath));
@@ -44,8 +44,14 @@ public class Blob {
 
     }
 
+    public String getHashString() {
+        return hashFileString;
+    }
+
+    // To-do: ask is this ho wthis is supposed to work
     public String hash(File file) throws Exception {
 
+        // hashes an entire file, not just insides
         try {
             MessageDigest md = MessageDigest.getInstance("SHA1");
             FileInputStream fis = new FileInputStream(file);
--- Index.java
+++ Index.java
@@ -5,8 +5,32 @@ import java.nio.file.*;
 
 public class Index {
 
+    // To-do: must ask about how to find target location
+    String indexPath = ".\\test";
+
     public Index() {
 
     }
 
+    public void init() throws Exception {
+        // create index file
+        File indexFile = new File(indexPath, "index");
+        indexFile.createNewFile();
+
+        // create objects folder
+        File objectsFolder = new File(indexPath, "objects");
+        objectsFolder.mkdirs();
+    }
+
+    public void add(File file) {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Index indexTest = new Index();
+
+        indexTest.init();
+
+        System.out.println("tester done");
+    }
 }
