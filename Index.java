ba56155e5b6ddcd4214dfff68d9b03d93ada6c2b
BenR <90284651+AsianUser@users.noreply.github.com>
2023-09-10 20:35:40 -0700

    add and remove method for Index
    writes new file to index and creates new blob
    
    removes text from index file

--- Index.java
+++ Index.java
@@ -30,7 +30,57 @@ public class Index {
         objectsFolderPath = objectsFolder.getPath();
     }
 
-    public void add(File file) {
+    public void add(String fileName) throws Exception {
+        Blob addBlob = new Blob(testFolderPath + "\\" + fileName);
+
+        String hash = addBlob.getHashString();
+
+        // write to index
+        FileWriter fw = new FileWriter(indexPath, true);
+        fw.write(fileName + " : " + hash + "\n");
+
+        System.out.println(fileName + " : " + hash + "\n");
+
+        fw.close();
+    }
+
+    public void remove(String fileName) throws Exception {
+
+        // look for fileName, takes hash, and delete line
+
+        System.out.println(indexFile.canRead());
+        BufferedReader bf = new BufferedReader(new FileReader(indexFile));
+
+        // boolean found = false;
+        String text = "";
+        while (bf.ready()) {
+            System.out.println("ready");
+            String line = bf.readLine();
+            // System.out.println(line.length());
+
+            // locate name
+            int index = 0;
+            while (line.charAt(index) != ':' && index < line.length()) {
+                // System.out.print(line.charAt(index) + "]]]");
+                index++;
+            }
+
+            if (!fileName.equals(line.substring(0, index - 1))) {
+                // System.out.println(line.substring(0, index - 1));
+                text += line + "\n";
+            }
+        }
+
+        // delete indexfile & rewrite in new
+        indexFile.delete();
+        indexFile.createNewFile();
+
+        FileWriter fw = new FileWriter(indexFile);
+        System.out.println("write");
+        fw.write(text);
+
+        bf.close();
+        fw.close();
 
     }
 
@@ -38,6 +88,12 @@ public class Index {
         Index indexTest = new Index();
 
         indexTest.init();
+
+        indexTest.add("test.txt");
+        indexTest.add("duplicate.txt");
+        indexTest.add("test2.txt");
+
+        indexTest.remove("test.txt");
 
         System.out.println("tester done");
     }
