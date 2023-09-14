0650365e5b57ab2e2e63170b7e3ef20431ef1f9b
Ronan Valle <ronanjvalle@gmail.com>
2023-09-13 22:09:57 -0700

    Fixed writeToFile
    made sure to use a SHA function on all contents to name the file

--- test/Tree.java
+++ test/Tree.java
@@ -4,6 +4,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +26,11 @@ public class Tree {
     }
 
     public void writeToFile(String fileName) throws IOException, NoSuchAlgorithmException {
+        String combinedContents = "";
+        for (String entry : entries) {
+            combinedContents += entry + "\n";
+        }
+        fileName = generateSHA(combinedContents);
         File file = new File("objects/" + fileName);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
             for (int i = 0; i < entries.size(); i++) {
@@ -32,6 +39,37 @@ public class Tree {
                     writer.newLine();
                 }
             }
+        }
+    }
+
+    public static String generateSHA(String input) throws NoSuchAlgorithmException {
+        try {
+            // getInstance() method is called with algorithm SHA-1
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+            // digest() method is called
+            // to calculate message digest of the input string
+            // returned as array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+
+            // Add preceding 0s to make it 32 bit
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+            // return the HashText
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
         }
     }
 }
