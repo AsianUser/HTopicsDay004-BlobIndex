1274178aed497ae62c8c4f66ca2eaac5f145648e
Ronan Valle <ronanjvalle@gmail.com>
2023-09-13 19:54:04 -0700

    Added remove and writeToFile
    remove method removes either a blob or tree entry from the tree. writeToFile method writes the entries stored in the tree into a file

--- test/Tree.java
+++ test/Tree.java
@@ -1,5 +1,10 @@
 package test;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,4 +19,19 @@ public class Tree {
         entries.add(entry);
     }
 
+    public void remove(String name) {
+        entries.removeIf(entry -> entry.contains(name));
+    }
+
+    public void writeToFile(String fileName) throws IOException, NoSuchAlgorithmException {
+        File file = new File("objects/" + fileName);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            for (int i = 0; i < entries.size(); i++) {
+                writer.write(entries.get(i));
+                if (i < entries.size() - 1) {
+                    writer.newLine();
+                }
+            }
+        }
+    }
 }
